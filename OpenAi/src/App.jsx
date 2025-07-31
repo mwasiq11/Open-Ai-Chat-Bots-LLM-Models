3ab9fa85fc13f4bfa69dc4af23d1b1c64abf910a
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-07-31 22:00:35 +0500

    creating Response/Completion component

--- OpenAi/src/App.jsx
+++ OpenAi/src/App.jsx
@@ -1,59 +1,13 @@
-import { useState } from "react";
-import "./App.css";
-const Api_key =
-  "place your api";
+import Response from "./Models/Response";
+import Completion from "./Models/Completion";
 
 function App() {
-  const [story, setStory] = useState("");
-  const [sentiment, setSentiment] = useState("");
-
-    async function callOpenAiApi() {
-      try {
-        const response = await fetch("https://api.openai.com/v1/responses", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${Api_key}`,
-          },
-          body: JSON.stringify({
-            model: "gpt-4.1",
-            input: "Tell me a three Sentence bedtime Story about a Unicorn.",
-          }),
-        });
-
-        if (!response.ok) {
-          const errorData = await response.json();
-          throw new Error(
-            `HTTP error! status: ${response.status}, message: ${
-              errorData.message || "Unknown error"
-            }`
-          );
-        }
-
-        const responseData = await response.json();
-        console.log("openai response", responseData);
-        return responseData;
-      } catch (error) {
-        console.error("Error during POST request:", error);
-        throw error;
-      }
-    }
-    console.log(story);
-
-
   return (
-    <div className="App">
-      <div>
-        <textarea
-          onChange={(e) => setStory(e.target.value)}
-          placeholder="Get the Story from Open Ai"
-          rows={10}
-          cols={50}
-        />
-      </div>
-      <button onClick={callOpenAiApi}>Tell me the Story</button>
-      {sentiment ? <h3>The Story is: {sentiment}</h3> : null}
+    <div className="flex">
+      <Completion />
+      <Response />
     </div>
   );
 }
+
 export default App;
---
+++ OpenAi/src/Models/Response.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+const Api_key =
+  "place your API";
+
+function Response() {
+  const [joke, setJoke] = useState("");
+  const [response, setResponse] = useState("");
+  const [loading,setLoading]=useState(false)
+  const callOpenAiApi = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    setResponse("")
+
+    try {
+      const response = await fetch("https://api.openai.com/v1/responses", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${Api_key}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-4o-mini",
+          input: "Tell me a three sentence bedtime story about a unicorn",
+        }),
+      });
+
+      if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(
+           ` HTTP error! status: ${response.status}, message: ${
+              errorData.message || "Unknown error"
+            }`
+          );
+        }
+      const responseData = await response.json();
+      console.log("openai response", responseData);
+      setResponse(responseData.choices[0].text);
+    } catch (error) {
+      console.error("Error in the Post request", error);
+      return error;
+    }
+  };
+  console.log(joke);
+  return (
+    <div className="App">
+        <form type="submit">
+      <div>
+        <textarea  
+        style={{margin:"2rem"}}
+        value={joke}
+          placeholder="Enter your prompt..."
+          onChange={(e) => setJoke(e.target.value)}
+          rows={7}
+          cols={40}
+        />
+      </div>
+      <button type="submit" disabled={loading} style={{marginLeft:"4rem"}} onClick={callOpenAiApi}>
+      {loading?"Loading...":"Submit"}
+      </button >
+      </form>
+      <br />
+
+      <textarea
+      readOnly
+      style={{marginLeft:"0.5rem"}}
+      value={response}
+       cols={50}
+       rows={15}/>
+    </div>
+  );
+}
+
+export default Response;
